fcbf36a54520ab02a380b605d8540a323d64276f
Asus-PC\Asus <sdoveh@gmail.com>
2019-02-19 14:20:19 +0200

    typo

--- cnn/architect_gp.py
+++ cnn/architect_gp.py
@@ -45,7 +45,7 @@ class Architect(object):
     loss.backward()
 
   def _backward_step_unrolled(self, input_train, target_train, input_valid, target_valid, eta, network_optimizer, prune_args):#calculates update rule for alpha
-    unrolled_model = self._compute_unroled_model(input_train, target_train, eta, network_optimizer)
+    unrolled_model = self._compute_unrolled_model(input_train, target_train, eta, network_optimizer)
     unrolled_loss = unrolled_model._loss(input_valid, target_valid)# L_val  on model with w'
 
     unrolled_loss.backward()#
